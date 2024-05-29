d41e4a1d8c630c477fdac91dc961b938ad5bea89
Miguel Angel Duran <mados_elyon7@hotmail.com>
2024-05-29 16:51:35 -0700

    inicio del proyecto

---
+++ src/main/java/com/aluracursos/desafio_libreria/DesafioLibreriaApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio_libreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLibreriaApplication.class, args);
+	}
+
+}
